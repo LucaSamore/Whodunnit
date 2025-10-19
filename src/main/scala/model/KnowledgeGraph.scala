9f2beaeaa70c766c0421ed66a22552778383a2d6
LucaSamore <luca.samore.01@gmail.com>
2025-10-19 11:31:53 +0200

    test: graph add node (refs #17)

--- src/main/scala/model/KnowledgeGraph.scala
+++ src/main/scala/model/KnowledgeGraph.scala
@@ -6,9 +6,13 @@ trait Graph:
   type Node
   type Edge
 
+  def nodes: Set[Node]
+  def addNode(n: Node): Unit
   def isEmpty: Boolean
 
 abstract class BaseGraph extends Graph:
   private val data = mutable.Map[Node, List[(Node, Edge)]]()
 
+  override def nodes: Set[Node] = data.keys.toSet
+  override def addNode(n: Node): Unit = data addOne n -> List()
   override def isEmpty: Boolean = data.isEmpty
--- src/test/scala/model/KnowledgeGraphTest.scala
+++ src/test/scala/model/KnowledgeGraphTest.scala
@@ -6,5 +6,21 @@ import org.scalatest.matchers.should.Matchers
 class KnowledgeGraphTest extends AnyFlatSpec with Matchers:
 
   "A BaseGraph" must "be initially empty" in:
-    val g = new BaseGraph {}
+    val g = new BaseGraph:
+      override type Node = Unit
+      override type Edge = Unit
     g.isEmpty shouldBe true
+
+  "An empty BaseGraph" must "not be empty after adding one Node" in:
+    val g = new BaseGraph:
+      override type Node = Int
+      override type Edge = Unit
+    g.addNode(1)
+    g.isEmpty shouldBe false
+
+  it must "contain one node" in:
+    val g = new BaseGraph:
+      override type Node = Int
+      override type Edge = Unit
+    g.addNode(1)
+    g.nodes.size shouldBe 1
