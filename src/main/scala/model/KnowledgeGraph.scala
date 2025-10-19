7f09c0939d186872f72e177963e76d6ab3fb33ff
LucaSamore <luca.samore.01@gmail.com>
2025-10-19 12:37:11 +0200

    test: graph add edge and out edges (refs #17)

--- src/main/scala/model/KnowledgeGraph.scala
+++ src/main/scala/model/KnowledgeGraph.scala
@@ -8,11 +8,23 @@ trait Graph:
 
   def nodes: Set[Node]
   def addNode(n: Node): Unit
+  def addEdge(n1: Node, e: Edge, n2: Node): Unit
+  def outEdges(n: Node): Set[Edge]
   def isEmpty: Boolean
 
 abstract class BaseGraph extends Graph:
   private val data = mutable.Map[Node, List[(Node, Edge)]]()
 
   override def nodes: Set[Node] = data.keys.toSet
+
   override def addNode(n: Node): Unit = data addOne n -> List()
+
+  override def addEdge(n1: Node, e: Edge, n2: Node): Unit =
+    if data.contains(n1) && data.contains(n2) && !outEdges(n1).contains(e) then
+      data.update(n1, (n2, e) :: data(n1))
+
+  override def outEdges(n: Node): Set[Edge] = data.get(n) match
+    case Some(edges) => edges.map(_._2).toSet
+    case _           => Set.empty
+
   override def isEmpty: Boolean = data.isEmpty
--- src/test/scala/model/KnowledgeGraphTest.scala
+++ src/test/scala/model/KnowledgeGraphTest.scala
@@ -11,7 +11,7 @@ class KnowledgeGraphTest extends AnyFlatSpec with Matchers:
       override type Edge = Unit
     g.isEmpty shouldBe true
 
-  "An empty BaseGraph" must "not be empty after adding one Node" in:
+  "An empty BaseGraph" must "not be empty after adding one node" in:
     val g = new BaseGraph:
       override type Node = Int
       override type Edge = Unit
@@ -24,3 +24,47 @@ class KnowledgeGraphTest extends AnyFlatSpec with Matchers:
       override type Edge = Unit
     g.addNode(1)
     g.nodes.size shouldBe 1
+
+  "Out edges" should "be empty if the node is orphan" in:
+    val g = new BaseGraph:
+      override type Node = Int
+      override type Edge = String
+    g.addNode(1)
+    g.outEdges(1).isEmpty shouldBe true
+
+  it should "not be empty if the node is not orphan" in:
+    val g = new BaseGraph:
+      override type Node = Int
+      override type Edge = String
+    g.addNode(1)
+    g.addNode(2)
+    g.addEdge(1, "test", 2)
+    g.outEdges(1).isEmpty shouldBe false
+
+  "Adding an edge" should "connect a node with another" in:
+    val g = new BaseGraph:
+      override type Node = Int
+      override type Edge = String
+    g.addNode(1)
+    g.addNode(2)
+    g.addEdge(1, "test", 2)
+    g.outEdges(1).contains("test") shouldBe true
+
+  it should "not connect non existing nodes" in:
+    val g = new BaseGraph:
+      override type Node = Int
+      override type Edge = String
+    g.addNode(1)
+    g.addNode(2)
+    g.addEdge(1, "test", 3)
+    g.outEdges(1).contains("test") shouldBe false
+
+  it should "not be done if the edge between the given nodes already exist" in:
+    val g = new BaseGraph:
+      override type Node = Int
+      override type Edge = String
+    g.addNode(1)
+    g.addNode(2)
+    g.addEdge(1, "test", 2)
+    g.addEdge(1, "test", 2)
+    g.outEdges(1).size shouldBe 1
