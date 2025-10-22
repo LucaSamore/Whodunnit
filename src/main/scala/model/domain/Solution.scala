b5e58019996fa55d111f7fed7b37ab5e30d6b307
Roberto Mitugno <roberto.mitugno@studio.unibo.it>
2025-10-22 10:37:55 +0200

    feat(case-generation): add CaseSolution implementation with KG prerequisite

--- src/main/scala/model/domain/Solution.scala
+++ src/main/scala/model/domain/Solution.scala
@@ -3,3 +3,15 @@ package model.domain
 sealed trait Solution:
   def culprit: Character
   def motive: String
+
+case class CaseSolution(
+    prerequisite: Set[KGPrerequisite],
+    culprit: Character,
+    motive: String
+) extends Solution
+
+case class KGPrerequisite(
+    firstEntity: Character | CaseFile,
+    secondEntity: Character | CaseFile,
+    semantic: String
+)
