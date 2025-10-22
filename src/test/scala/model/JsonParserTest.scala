ec4b0a4793296ecdedd7de1e1929b699b861f6b5
Roberto Mitugno <roberto.mitugno@studio.unibo.it>
2025-10-22 13:29:12 +0200

    test(case-generation): add failing test for plot extraction

---
+++ src/test/scala/model/JsonParserTest.scala
@@ -0,0 +1,19 @@
+package model
+
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers
+
+import scala.util.Success
+
+class JsonParserTest extends AnyWordSpec with Matchers:
+
+  "JsonParser" when:
+    "parsing plot field" should:
+      "extract plot text from valid JSON" in:
+        val json = """{"plot": "A mysterious case"}"""
+
+        val parser = Parser()
+        val result = parser.parse(json)
+
+        result shouldBe a[Success[_]]
+        result.get.plot.text shouldBe "A mysterious case"
