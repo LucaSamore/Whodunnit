e99e9aff17c635a36970d1290ed38650cfc546aa
Roberto Mitugno <roberto.mitugno@studio.unibo.it>
2025-10-22 16:15:46 +0200

    test(case-generation): add test handle missing plot field

--- src/test/scala/model/JsonParserTest.scala
+++ src/test/scala/model/JsonParserTest.scala
@@ -17,3 +17,14 @@ class JsonParserTest extends AnyWordSpec with Matchers:
 
         result shouldBe a[Success[_]]
         result.get.plot.text shouldBe "A mysterious case"
+
+      "handle missing plot field" in:
+        val invalidJson = """{"characters": []}"""
+        
+        val jsonParser: Parser = JsonParser
+        val result = jsonParser.parse(invalidJson)
+        
+        result shouldBe a[Left[_, _]]
+        result.left.value shouldBe a[MissingFieldError]
+        result.left.value.message should include("plot")
+        
