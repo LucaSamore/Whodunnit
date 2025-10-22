8a89d82b4619b03cdb56abd49dd295f8ff0fba80
Roberto Mitugno <roberto.mitugno@studio.unibo.it>
2025-10-22 10:00:54 +0200

    feat(case-generation): add CaseFile to Entity hierarchy

--- src/main/scala/model/domain/Entity.scala
+++ src/main/scala/model/domain/Entity.scala
@@ -1,10 +1,22 @@
 package model.domain
 
 import model.domain.types.CaseRole
+import model.domain.types.CaseFileType
+
+import java.util.Date
 
 sealed trait Entity
 
 case class Character(
     name: String,
     role: CaseRole
+) extends Entity
+
+case class CaseFile(
+    title: String,
+    content: String,
+    kind: CaseFileType,
+    sender: Option[Character],
+    receiver: Option[Character],
+    date: Option[Date]
 ) extends Entity
---
+++ src/main/scala/model/domain/types/CaseFileType.scala
@@ -0,0 +1,9 @@
+package model.domain.types
+
+enum CaseFileType:
+  case Message
+  case Email
+  case Interview
+  case Diary
+  case TextDocument
+  case Notes
