991b9cabbe7391d5df5ba58601e89e77b1559473
Roberto Mitugno <roberto.mitugno@studio.unibo.it>
2025-10-23 20:44:23 +0200

    test(case-generation): add tests for GenerationError

--- src/test/scala/model/case_generation/CaseGeneratorTest.scala
+++ src/test/scala/model/case_generation/CaseGeneratorTest.scala
@@ -6,12 +6,28 @@ import org.scalatest.EitherValues
 
 
 class CaseGeneratorTest extends AnyWordSpec with Matchers with EitherValues:
-  
+
   "CaseGenerator" should :
     "have generate method returning Either" in :
       val generator: CaseGenerator = new CaseGenerator:
         def generate(constraints: Constraint*): Either[Error, Case] =
-          Left(Error.InvalidPromptError("Not implemented"))
+          Left(Error("Not implemented"))
 
       val result = generator.generate(Constraint.Theme("Test"))
       result shouldBe a[Left[_, _]]
+
+
+  "GenerationError" should :
+    "have LLMError with message" in :
+      val error = GenerationError.LLMError("Connection timeout")
+      error.message shouldBe "Connection timeout"
+
+    "have InvalidPromptError with message" in :
+      val error = GenerationError.InvalidPromptError("No constraints provided")
+      error.message shouldBe "No constraints provided"
+
+    "have ParseFailureError wrapping ParseError" in :
+      val parseError = MissingFieldError("plot")
+      val error = GenerationError.ParseFailureError(parseError)
+
+      error.message should include("plot")
