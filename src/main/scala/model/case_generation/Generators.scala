626ecfed5e071114ef6d91ab836f20133bf0e057
Roberto Mitugno <roberto.mitugno@studio.unibo.it>
2025-10-24 00:55:44 +0200

    feat(case-generation): implement default case generator using GroqLLMService

--- src/main/scala/model/case_generation/Generators.scala
+++ src/main/scala/model/case_generation/Generators.scala
@@ -1,5 +1,10 @@
 package model.case_generation
 
-class Generators {
+object Generators:
+  given groqCaseGenerator: CaseGenerator = defaultGenerator
+  
+  private lazy val defaultGenerator: CaseGenerator =
+    GroqLLMService.fromEnv() match
+      case Right(service) => LLMCaseGenerator(service, JsonParser)
+      case Left(error) => throw IllegalStateException(error.message)
 
-}
