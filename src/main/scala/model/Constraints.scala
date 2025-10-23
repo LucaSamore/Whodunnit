a51ee26165f417ef74a42e161c7c8b676ab8fb12
Roberto Mitugno <roberto.mitugno@studio.unibo.it>
2025-10-23 18:40:02 +0200

    feat(case-generation): implement Theme constraint

--- src/main/scala/model/Constraints.scala
+++ src/main/scala/model/Constraints.scala
@@ -3,15 +3,4 @@ package model
 sealed trait Constraints
 
 object Constraints:
-  final case class Theme(value: String) extends Constraints
-
-  sealed trait ConstraintError:
-    def message: String
-
-  case class EmptyTheme(message: String = "Theme cannot be empty") extends ConstraintError
-
-  object validated:
-    def theme(value: String): Either[ConstraintError, Theme] =
-      if value.trim.isEmpty then Left(EmptyTheme())
-      else
-        Right(Theme(value))
+  case class Theme(value: String) extends Constraints
