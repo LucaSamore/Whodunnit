2cb08053b4f4e01ca774b86770ff230134c4c993
Roberto Mitugno <roberto.mitugno@studio.unibo.it>
2025-10-23 18:42:47 +0200

    feat(case-generation): implement CaseFilesRange constraint

--- src/main/scala/model/Constraints.scala
+++ src/main/scala/model/Constraints.scala
@@ -5,3 +5,5 @@ sealed trait Constraints
 object Constraints:
   case class Theme(value: String) extends Constraints
   case class CharactersRange(min: Int, max: Int) extends Constraints
+  case class CaseFilesRange(min: Int, max: Int) extends Constraints
+
