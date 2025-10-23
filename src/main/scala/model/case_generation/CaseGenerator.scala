be46c09670f04a199a8dfe78946d10bc22236482
Roberto Mitugno <roberto.mitugno@studio.unibo.it>
2025-10-23 20:46:50 +0200

    feat(generation): implement GenerationError

--- src/main/scala/model/case_generation/CaseGenerator.scala
+++ src/main/scala/model/case_generation/CaseGenerator.scala
@@ -2,3 +2,15 @@ package model.case_generation
 
 trait CaseGenerator:
   def generate(constraints: Constraint*): Either[Error, Case]
+
+
+sealed trait GenerationError:
+  def message: String
+
+object GenerationError:
+  case class LLMError(message: String) extends GenerationError
+
+  case class InvalidPromptError(message: String) extends GenerationError
+
+  case class ParseFailureError(parseError: ParseError) extends GenerationError:
+    def message: String = s"Failed to parse LLM response: ${parseError.message}"
