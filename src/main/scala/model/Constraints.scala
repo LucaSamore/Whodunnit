6199cd7d53397eed93a7bf0bdab435cc1f1b06aa
Roberto Mitugno <roberto.mitugno@studio.unibo.it>
2025-10-23 18:44:16 +0200

    feat(case-generation): implement PrerequisitesRange constraint

--- src/main/scala/model/Constraints.scala
+++ src/main/scala/model/Constraints.scala
@@ -6,4 +6,6 @@ object Constraints:
   case class Theme(value: String) extends Constraints
   case class CharactersRange(min: Int, max: Int) extends Constraints
   case class CaseFilesRange(min: Int, max: Int) extends Constraints
+  case class PrerequisitesRange(min: Int, max: Int) extends Constraints
+
 
