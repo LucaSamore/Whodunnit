1cb0a5f8f9c694c459ba962a06aed948b277b715
Roberto Mitugno <roberto.mitugno@studio.unibo.it>
2025-10-24 11:54:30 +0200

    refactor(case-generation): move Solution trait and CaseSolution inside Case file

--- src/main/scala/model/case_generation/Case.scala
+++ src/main/scala/model/case_generation/Case.scala
@@ -28,3 +28,19 @@ object Case:
       characters: Set[Character],
       solution: Solution
   ) extends Case
+
+sealed trait Solution:
+  def culprit: Character
+  def motive: String
+
+final case class CaseSolution(
+    prerequisite: Set[KGPrerequisite],
+    culprit: Character,
+    motive: String
+) extends Solution
+
+final case class KGPrerequisite(
+    firstEntity: Character | CaseFile,
+    secondEntity: Character | CaseFile,
+    semantic: String
+)
