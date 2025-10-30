1f577103eba2c9e4c6ad57eb892d207023e662e0
Lucia Castellucci <lucia.castellucci2@studio.unibo.it>
2025-10-30 18:35:50 +0100

    test(versioning): base mutable buffer operations (refs #22)

--- src/test/scala/model/versioning/BufferTest.scala
+++ src/test/scala/model/versioning/BufferTest.scala
@@ -5,12 +5,34 @@ import org.scalatest.wordspec.AnyWordSpec
 
 class BufferTest extends AnyWordSpec with Matchers:
 
-  "A Buffer" when:
+  "A mutable Buffer" when:
     "newly created" should:
-      val buffer = Buffer(5, List.empty[Int])
+      val maxSize = 5
+      val buffer = Buffer[Int](maxSize, new Array(maxSize))
 
       "have size 0" in:
-        buffer.size shouldBe 0
+        buffer.currentSize shouldBe 0
 
-      "be empty" in:
+      "is empty" in:
         buffer.isEmpty shouldBe true
+
+    "elements added" should:
+      val maxSize = 5
+      val buffer = Buffer[Int](maxSize, new Array(maxSize))
+      buffer.add(1)
+      buffer.add(2)
+      buffer.add(3)
+
+      "have correct size" in:
+        buffer.currentSize shouldBe 3
+
+      "not be empty" in:
+        buffer.isEmpty shouldBe false
+
+      "contain the added elements" in:
+        buffer.contains(1) shouldBe true
+        buffer.contains(2) shouldBe true
+        buffer.contains(3) shouldBe true
+
+      "have elements in correct order" in:
+        buffer.elementList shouldBe List(1, 2, 3)
