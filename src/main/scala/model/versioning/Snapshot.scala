418c3280243875bb3c46f82f7a0120d7eae7c926
Lucia Castellucci <lucia.castellucci2@studio.unibo.it>
2025-10-30 17:36:41 +0100

    feat(versioning): refactor to allow base snapshot of mutable object (refs #22)

--- src/main/scala/model/versioning/Snapshot.scala
+++ src/main/scala/model/versioning/Snapshot.scala
@@ -2,20 +2,67 @@ package model.versioning
 
 import java.time.LocalDateTime
 
-class Snapshot[A](val subject: A, val timestamp: LocalDateTime)
+trait Snapshot[+A]:
+  def subject: A
+
+trait Snapshottable[S]:
+  def snap(s: S, timestamp: LocalDateTime): Snapshot[S]
+  def restore(snapshot: Snapshot[S]): S
 
 object Snapshot:
-  def snap[A](subject: A): Snapshot[A] = {
-    new Snapshot(subject, LocalDateTime.now())
-  }
+  def apply[S: Snapshottable](
+      s: S,
+      timestamp: LocalDateTime = LocalDateTime.now
+  ): Snapshot[S] =
+    summon[Snapshottable[S]].snap(s, timestamp)
 
-  def restore[A](snapshot: Snapshot[A]): A = {
-    snapshot.subject
-  }
+  def restore[S: Snapshottable](snapshot: Snapshot[S]): S =
+    summon[Snapshottable[S]].restore(snapshot)
+
+  private case class SnapshotImpl[A](
+      override val subject: A,
+      timestamp: LocalDateTime
+  ) extends Snapshot[A]
+
+  object Snapshotters:
+    given Snapshottable[Int] with
+      def snap(value: Int, timestamp: LocalDateTime): Snapshot[Int] =
+        SnapshotImpl(value, timestamp)
+
+      def restore(snapshot: Snapshot[Int]): Int =
+        snapshot.subject
+
+    given Snapshottable[String] with
+      def snap(value: String, timestamp: LocalDateTime): Snapshot[String] =
+        SnapshotImpl(value, timestamp)
+
+      def restore(snapshot: Snapshot[String]): String =
+        snapshot.subject
+
+    given Snapshottable[History] with
+      def snap(history: History, timestamp: LocalDateTime): Snapshot[History] =
+        SnapshotImpl(history.deepCopy(), timestamp)
+
+      def restore(snapshot: Snapshot[History]): History =
+        snapshot.subject.deepCopy()
+
+    given Snapshottable[MutableHistory] with
+      def snap(
+          history: MutableHistory,
+          timestamp: LocalDateTime
+      ): Snapshot[MutableHistory] =
+        SnapshotImpl(history.deepCopy(), timestamp)
+
+      def restore(snapshot: Snapshot[MutableHistory]): MutableHistory =
+        snapshot.subject.deepCopy()
 
 case class History(elements: List[Int]) {
   def add(element: Int): History = {
     History(elements :+ element)
+  }
+
+  def deepCopy(): History = {
+    History(elements.map(identity))
   }
 }
 
@@ -23,5 +70,9 @@ case class MutableHistory(elements: Array[Int], var size: Int = 0) {
   def add(element: Int): Unit = {
     elements(size) = element
     size += 1
+  }
+
+  def deepCopy(): MutableHistory = {
+    MutableHistory(elements.slice(0, size), size)
   }
 }
--- src/test/scala/model/versioning/SnapshotTest.scala
+++ src/test/scala/model/versioning/SnapshotTest.scala
@@ -1,30 +1,29 @@
 package model.versioning
 
+import model.versioning.Snapshot.Snapshotters.{
+  given_Snapshottable_History,
+  given_Snapshottable_Int,
+  given_Snapshottable_MutableHistory,
+  given_Snapshottable_String
+}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
-import java.time.LocalDateTime
 
 class SnapshotTest extends AnyWordSpec with Matchers:
   "A Snapshot" when:
     "created with value" should:
       "contain the original value" in:
-        val snapshot = Snapshot.snap(3)
+        val snapshot = Snapshot(3)
         snapshot.subject shouldBe 3
 
       "preserve the value type" in:
-        val snapshot = Snapshot.snap("Hello World")
+        val snapshot = Snapshot("Hello World")
         snapshot.subject shouldBe "Hello World"
-
-      "use current time as default" in:
-        val before = LocalDateTime.now
-        val snapshot = Snapshot.snap(3)
-        val after = LocalDateTime.now
-        snapshot.timestamp should (be >= before and be <= after)
 
     "restored" should:
       "return the original value" in:
         val original = 42
-        val snapshot = Snapshot.snap(original)
+        val snapshot = Snapshot(original)
         val restored = Snapshot.restore(snapshot)
 
         restored shouldBe original
@@ -32,21 +31,21 @@ class SnapshotTest extends AnyWordSpec with Matchers:
     "created with an immutable object" should:
       "capture the current state" in:
         val history = History(List.empty)
-        val snapshot = Snapshot.snap(history.add(3))
+        val snapshot = Snapshot(history.add(3))
 
         snapshot.subject.elements should contain(3)
 
       "not be affected by subsequent changes" in:
         val history = History(List.empty)
-        val snapshot = Snapshot.snap(history.add(3))
+        val snapshot = Snapshot(history.add(3))
         history.add(3)
 
         snapshot.subject.elements should contain only 3
 
       "isolate multiple snapshots from each other" in:
         val history = History(List.empty)
-        val snapshot1 = Snapshot.snap(history.add(3))
-        val snapshot2 = Snapshot.snap(history.add(3).add(5))
+        val snapshot1 = Snapshot(history.add(3))
+        val snapshot2 = Snapshot(history.add(3).add(5))
 
         snapshot1.subject.elements.size shouldBe 1
         snapshot2.subject.elements.size shouldBe 2
@@ -55,14 +54,14 @@ class SnapshotTest extends AnyWordSpec with Matchers:
       "capture the current state" in:
         val history = MutableHistory(new Array(5))
         history.add(3)
-        val snapshot = Snapshot.snap(history)
+        val snapshot = Snapshot(history)
 
         snapshot.subject.elements should contain(3)
 
       "not be affected by subsequent changes" in:
         val history = MutableHistory(new Array(5))
         history.add(3)
-        val snapshot = Snapshot.snap(history)
+        val snapshot = Snapshot(history)
         history.add(3)
 
         snapshot.subject.elements should contain only 3
@@ -70,9 +69,9 @@ class SnapshotTest extends AnyWordSpec with Matchers:
       "isolate multiple snapshots from each other" in:
         val history = MutableHistory(new Array(5))
         history.add(3)
-        val snapshot1 = Snapshot.snap(history)
+        val snapshot1 = Snapshot(history)
         history.add(5)
-        val snapshot2 = Snapshot.snap(history)
+        val snapshot2 = Snapshot(history)
 
         snapshot1.subject.elements.length shouldBe 1
         snapshot2.subject.elements.length shouldBe 2
