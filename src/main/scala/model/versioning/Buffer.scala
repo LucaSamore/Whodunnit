7d4cfa6b5e2567c7ecd4137e3063e360645f7ea1
Lucia Castellucci <lucia.castellucci2@studio.unibo.it>
2025-10-30 18:45:42 +0100

    feat(versioning): base mutable buffer operations (refs #22)

--- src/main/scala/model/versioning/Buffer.scala
+++ src/main/scala/model/versioning/Buffer.scala
@@ -1,7 +1,15 @@
 package model.versioning
 
-case class Buffer[T](capacity: Int, elements: List[T]):
+case class Buffer[T](capacity: Int, elements: Array[T]):
+  private var size: Int = 0
 
-  def size: Int = elements.size
+  def currentSize: Int = size
 
-  def isEmpty: Boolean = elements.isEmpty
+  def isEmpty: Boolean = size == 0
+
+  def add(element: T): Unit = {
+    elements(size) = element
+    size += 1
+  }
+
+  def contains(element: T): Boolean = elements.contains(element)
