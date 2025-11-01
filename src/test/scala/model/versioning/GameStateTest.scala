a783a120ce6e06fea5ba45692630956212a16568
Lucia Castellucci <lucia.castellucci2@studio.unibo.it>
2025-11-01 13:23:35 +0100

    test(versioning): add state to game history (refs #22)

--- src/test/scala/model/versioning/GameStateTest.scala
+++ src/test/scala/model/versioning/GameStateTest.scala
@@ -21,3 +21,19 @@ class GameStateTest extends AnyWordSpec with Matchers:
       "be a deep copy (equal but distinct instance)" in:
         copiedHistory should not be theSameInstanceAs(originalHistory)
         copiedHistory shouldEqual originalHistory
+
+    "elements are added" should:
+      val maxSize = 3
+      val originalHistory = GameHistory(maxSize)
+      // Mock KnowledgeGraph instances
+      case class MockKnowledgeGraph(id: Int) extends KnowledgeGraph:
+        def deepCopy(): KnowledgeGraph = MockKnowledgeGraph(id)
+
+      val kg1 = MockKnowledgeGraph(1)
+      val kg2 = MockKnowledgeGraph(2)
+
+      originalHistory.addState(kg1)
+      originalHistory.addState(kg2)
+
+      "maintain the correct current state" in:
+        originalHistory.currentState shouldBe Some(kg2)
