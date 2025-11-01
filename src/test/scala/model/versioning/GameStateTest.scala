a1b5562c2b29e60d099dd5d92e9a51fceac5f09c
Lucia Castellucci <lucia.castellucci2@studio.unibo.it>
2025-11-01 14:44:45 +0100

    test(versioning): history navigation edge cases (refs #22)

--- src/test/scala/model/versioning/GameStateTest.scala
+++ src/test/scala/model/versioning/GameStateTest.scala
@@ -60,3 +60,35 @@ class GameStateTest extends AnyWordSpec with Matchers:
         originalHistory.undo()
         originalHistory.undo()
         originalHistory.redo() shouldBe Some(kg2)
+
+    "combination of undo and redo are called" should:
+      val maxSize = 3
+      val originalHistory = GameHistory(maxSize)
+      case class MockKnowledgeGraph(id: Int) extends KnowledgeGraph:
+        def deepCopy(): KnowledgeGraph = MockKnowledgeGraph(id)
+
+      val kg1 = MockKnowledgeGraph(1)
+      val kg2 = MockKnowledgeGraph(2)
+      val kg3 = MockKnowledgeGraph(3)
+      originalHistory.addState(kg1)
+      originalHistory.addState(kg2)
+      originalHistory.addState(kg3)
+
+      "work correctly" in:
+        originalHistory.redo() shouldBe None
+        originalHistory.currentState shouldBe Some(kg3)
+        originalHistory.undo() shouldBe Some(kg2)
+        originalHistory.redo() shouldBe Some(kg3)
+        originalHistory.currentState shouldBe Some(kg3)
+        originalHistory.redo() shouldBe None
+        originalHistory.currentState shouldBe Some(kg3)
+
+      "adding a new state after undo clears redo history" in:
+        originalHistory.undo() shouldBe Some(kg2)
+        originalHistory.undo() shouldBe Some(kg1)
+        val kg4 = MockKnowledgeGraph(4)
+        originalHistory.addState(kg4)
+        originalHistory.redo() shouldBe None // redo should not be possible anymore
+        originalHistory.currentState shouldBe Some(kg4)
+        originalHistory.undo() shouldBe Some(kg1)
+        originalHistory.undo() shouldBe None
