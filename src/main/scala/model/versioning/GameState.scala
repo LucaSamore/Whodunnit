8a05abc19d8f50485c6be60695d0d3a9a5eaa6d4
Lucia Castellucci <lucia.castellucci2@studio.unibo.it>
2025-11-01 13:16:26 +0100

    feat(versioning): game history deep copy (refs #22)

--- src/main/scala/model/versioning/GameState.scala
+++ src/main/scala/model/versioning/GameState.scala
@@ -9,6 +9,18 @@ case class GameHistory(
 ):
   def currentState: Option[KnowledgeGraph] = timeline.currentElement
 
+  def deepCopy(): GameHistory =
+    val newBuffer = RingNavigableBuffer[KnowledgeGraph](timeline.capacity)
+    timeline.elements.foreach(kg => newBuffer.push(kg.deepCopy()))
+    GameHistory(historySize, newBuffer)
+
+  override def equals(obj: Any): Boolean = obj match
+    case that: GameHistory =>
+      this.historySize == that.historySize &&
+      this.timeline.elements == that.timeline.elements &&
+      this.timeline.currentPosition == that.timeline.currentPosition
+    case _ => false
+
 object GameHistory:
   def apply(historySize: Int): GameHistory =
     GameHistory(historySize, RingNavigableBuffer[KnowledgeGraph](historySize))
