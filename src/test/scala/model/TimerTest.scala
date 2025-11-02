ca54e4ed73d2f2fc33b5917066ffab179b416662
Roberto Mitugno <roberto.mitugno@studio.unibo.it>
2025-11-02 18:35:47 +0100

    test(timer): add test for formatting duration in Timer (refs #65)

--- src/test/scala/model/TimerTest.scala
+++ src/test/scala/model/TimerTest.scala
@@ -71,3 +71,21 @@ class TimerTest extends AnyWordSpec with Matchers:
 
         newState shouldBe Finished
         remainingTime shouldBe Some(Duration.Zero)
+
+    "formatting duration" should:
+      "format zero seconds correctly" in:
+        val duration = 0.seconds
+        Timer.formatDuration(duration) shouldBe "00:00"
+      
+      "format seconds correctly" in:
+        val duration = 45.seconds
+        Timer.formatDuration(duration) shouldBe "00:45"
+      
+      "format minutes and seconds correctly" in:
+        val duration = 125.seconds
+        Timer.formatDuration(duration) shouldBe "02:05"
+
+      "format exact minutes correctly" in:
+        val duration = 3.minutes
+        Timer.formatDuration(duration) shouldBe "03:00"
+      
