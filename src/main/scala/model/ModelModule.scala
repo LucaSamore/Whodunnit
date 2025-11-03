13ca8765dd304c8078b51b7d13f99b5c5ee9c744
Lucia Castellucci <lucia.castellucci2@studio.unibo.it>
2025-11-03 23:07:01 +0100

    feat: base architecture for mvc

---
+++ src/main/scala/model/ModelModule.scala
@@ -0,0 +1,27 @@
+package model
+
+import model.casegeneration.*
+
+object ModelModule:
+
+  // Placeholder State class
+  // TODO: Replace with actual state properties
+  case class State(
+      currentCase: Option[Case] = None,
+      knowledgeGraph: Option[CaseKnowledgeGraph] = None,
+      isLoading: Boolean = false,
+      error: Option[String] = None
+  )
+
+  trait Model[S]:
+    def createNothing(): Unit
+
+  trait Provider[S]:
+    val model: Model[S]
+
+  trait Component[S]:
+    class ModelImpl extends Model[S]:
+      override def createNothing(): Unit = println("Creating nothing...")
+
+  trait Interface[S] extends Provider[S] with Component[S]:
+    def Model(): Model[S] = new ModelImpl()
