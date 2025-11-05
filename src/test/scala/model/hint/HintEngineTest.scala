11b93a265c8a60c2ff55a6746dfa92f8731334bc
LucaSamore <luca.samore.01@gmail.com>
2025-11-05 22:41:08 +0100

    test(hint): add and operator test of rule dsl (refs #26)

--- src/test/scala/model/hint/HintEngineTest.scala
+++ src/test/scala/model/hint/HintEngineTest.scala
@@ -1,6 +1,6 @@
 package model.hint
 
-import model.hint.Metric.density
+import model.hint.Metric.{coverage, density}
 import model.hint.Trend.{Increasing, Stable, Worsening}
 import model.hint.TrendAnalyzers.simpleTrendAnalyzer
 import model.knowledgegraph.BaseOrientedGraph
@@ -28,7 +28,7 @@ class HintEngineTest extends AnyFlatSpec with Matchers with GivenWhenThen:
     )
 
     When("checking if density is increasing")
-    val check: MetricCheck[TestGraph] = when(density) is Increasing
+    val check: MetricCheck[TestGraph] = when(density) == Increasing
 
     Then("the check should evaluate to true")
     check.eval(history) shouldBe true
@@ -38,7 +38,21 @@ class HintEngineTest extends AnyFlatSpec with Matchers with GivenWhenThen:
     val history = List(graph.withNodes(1, 2, 3))
 
     When("checking if density is increasing")
-    val check = when(density) is Increasing
+    val check = when(density) == Increasing
 
     Then("the check should evaluate to false")
     check.eval(history) shouldBe false
+
+  "MetricCheck combinators" should "work with 'and' operator" in:
+    Given("a history with stable coverage and worsening density")
+    val history = List(
+      graph.withNodes(1, 2, 3).withEdge(1, "link1", 2).withEdge(2, "link2", 3),
+      graph.withNodes(1, 2, 3).withEdge(1, "link1", 2),
+      graph.withNodes(1, 2, 3)
+    )
+
+    When("checking both conditions with 'and'")
+    val check = when(coverage) == Stable and when(density) == Worsening
+
+    Then("the combined check should evaluate to true")
+    check.eval(history) shouldBe true
