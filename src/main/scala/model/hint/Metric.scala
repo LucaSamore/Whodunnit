6d2a80836fe6368a54a5930805c7ca2d4417569f
LucaSamore <luca.samore.01@gmail.com>
2025-11-05 23:46:01 +0100

    feat(hint): implement coverage metric (refs #26)

--- src/main/scala/model/hint/Metric.scala
+++ src/main/scala/model/hint/Metric.scala
@@ -5,11 +5,31 @@ import model.knowledgegraph.BaseOrientedGraph
 object Metric:
   type MetricValue = Double
 
-  extension (bog: BaseOrientedGraph)
+  extension [G <: BaseOrientedGraph](graph: G)
     def density: MetricValue =
       // TODO: specify in the doc that if the graph is empty, we say the density is conventionally zero
-      val edgesCardinality = bog.edges.size
-      val nodesCardinality = bog.nodes.size
+      val edgesCardinality = graph.edges.size
+      val nodesCardinality = graph.nodes.size
       if (nodesCardinality > 0) then
         edgesCardinality.toDouble / nodesCardinality * (nodesCardinality - 1)
       else 0.0
+
+    private def coverage(other: G): MetricValue =
+      def ratio[A](source: Set[A], target: Set[A]): MetricValue =
+        if source.isEmpty then 1.0
+        else source.intersect(target).size.toDouble / source.size
+
+      (graph.isEmpty, other.isEmpty) match
+        case (true, _)     => 1.0
+        case (false, true) => 0.0
+        case _             =>
+          (ratio(
+            graph.nodes,
+            other.nodes.asInstanceOf[Set[graph.Node]]
+          ) + ratio(
+            graph.edges,
+            other.edges.asInstanceOf[Set[(graph.Node, graph.Edge, graph.Node)]]
+          )) / 2.0
+
+  def coverageFor[G <: BaseOrientedGraph](refGraph: G): G => MetricValue =
+    (graph: G) => graph.coverage(refGraph)
