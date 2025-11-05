f83ea36cc8150ce7e14fe5f86e3ab11e26930d1a
Lucia Castellucci <lucia.castellucci2@studio.unibo.it>
2025-11-05 21:35:28 +0100

    feat(case-generation): initialize game state (refs #12)

--- src/main/scala/model/GameState.scala
+++ src/main/scala/model/GameState.scala
@@ -1,8 +1,9 @@
 package model
 
 import model.casegeneration.Case
-import model.knowledgegraph.Graph
-import model.versioning.{History, TimeMachine}
+import model.knowledgegraph.{CaseKnowledgeGraph, Graph}
+import model.versioning.{GameHistory, GameTimeMachine, History, TimeMachine}
+import model.versioning.Snapshot.Snapshotters.given_Snapshottable_History
 
 // TODO: Define Timer and Hint properly
 type Timer = Int
@@ -20,3 +21,16 @@ case class GameState(
 object GameState:
   def empty(): GameState =
     GameState(None, None, None, None, None, None)
+
+  def initialize(
+      gameCase: Case,
+      timer: Timer
+  ): GameState =
+    GameState(
+      Some(gameCase),
+      Some(CaseKnowledgeGraph()),
+      Some(GameHistory(5)),
+      Some(GameTimeMachine[History](None)),
+      Some(Seq.empty),
+      Some(timer)
+    )
