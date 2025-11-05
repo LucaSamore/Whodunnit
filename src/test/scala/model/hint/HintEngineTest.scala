f9e26f2bfb078d4b5089446e6e146bc8f2492cb6
LucaSamore <luca.samore.01@gmail.com>
2025-11-05 21:08:18 +0100

    test(hint): a history with incrementing metric has an increasing trend (refs #26)

---
+++ src/test/scala/model/hint/HintEngineTest.scala
@@ -0,0 +1,34 @@
+package model.hint
+
+import model.hint.Metric.density
+import model.hint.Trend.{Increasing, Stable, Worsening}
+import model.hint.TrendAnalyzers.simpleTrendAnalyzer
+import model.knowledgegraph.BaseOrientedGraph
+import org.scalatest.GivenWhenThen
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+import scala.language.postfixOps
+
+class HintEngineTest extends AnyFlatSpec with Matchers with GivenWhenThen:
+
+  type TestGraph = BaseOrientedGraph { type Node = Int; type Edge = String }
+
+  private def graph: TestGraph =
+    new BaseOrientedGraph:
+      override type Node = Int
+      override type Edge = String
+
+  "MetricCheck" should "evaluate to true when condition matches" in:
+    Given("a history of graphs with incrementing density")
+    val history = List(
+      graph.withNodes(1, 2, 3),
+      graph.withNodes(1, 2, 3).withEdge(1, "link1", 2),
+      graph.withNodes(1, 2, 3).withEdge(1, "link1", 2).withEdge(2, "link2", 3)
+    )
+
+    When("checking if density is increasing")
+    val check: MetricCheck[TestGraph] = when(density) is Increasing
+
+    Then("the check should evaluate to true")
+    check.eval(history) shouldBe true
