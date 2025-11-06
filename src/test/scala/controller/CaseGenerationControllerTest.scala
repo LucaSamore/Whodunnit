552d451e893cb4877c99c95e120dc19abf1cd466
Lucia Castellucci <lucia.castellucci2@studio.unibo.it>
2025-11-06 19:07:12 +0100

    test(case-generation): fix after moving CaseGenerationController logics to ControllerModule (refs #12)

--- src/test/scala/controller/CaseGenerationControllerTest.scala
+++ src/test/scala/controller/CaseGenerationControllerTest.scala
@@ -1,9 +1,10 @@
 package controller
 
 import utils.TestUtils.mockCase
-import cats.effect.IO
 import cats.effect.unsafe.implicits.global
 import model.casegeneration.*
+import model.ModelModule
+import view.ViewModule
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.EitherValues
@@ -11,93 +12,97 @@ import org.scalatest.EitherValues
 class CaseGenerationControllerTest extends AnyWordSpec with Matchers
     with EitherValues:
 
-  class MockCaseGenerationModel(
-      response: Seq[Constraint] => IO[Either[ProductionError, Case]]
-  ) extends CaseGenerationModel:
+  class MockProducer(
+      response: Seq[Constraint] => Either[ProductionError, Case]
+  ) extends Producer[Case]:
     var lastConstraints: Seq[Constraint] = Seq.empty
 
-    override def generateCase(
-        constraints: Seq[Constraint]
-    ): IO[Either[ProductionError, Case]] =
-      lastConstraints = constraints
-      response(constraints)
+    override def produce(constraints: Constraint*)
+        : Either[ProductionError, Case] =
+      lastConstraints = constraints.toSeq
+      response(lastConstraints)
 
-  "CaseGenerationController" should:
+  def createTestEnvironment(mockProducer: MockProducer)
+      : (ModelModule.Model[Unit], ControllerModule.Controller[Unit]) =
+    val env = new ModelModule.Component[Unit]
+      with ControllerModule.Component[Unit]
+      with ViewModule.Component[Unit]
+      with ModelModule.Provider[Unit]
+      with ControllerModule.Provider[Unit]
+      with ViewModule.Provider[Unit]:
+
+      val model: ModelModule.Model[Unit] = new ModelImpl:
+        override val producer: Producer[Case] = mockProducer
+      val view: ViewModule.View[Unit] = new ViewImpl
+      val controller: ControllerModule.Controller[Unit] = new ControllerImpl
+    (env.model, env.controller)
+
+  "ModelModule case generation (accessed through Controller's Cake Pattern)" should:
     "include difficulty constraint in generateNewCase" in:
-      val model = MockCaseGenerationModel(_ => IO.pure(Right(mockCase)))
-      val controller = CaseGenerationController(model)
+      val mockProducer = new MockProducer(_ => Right(mockCase))
+      val (model, _) = createTestEnvironment(mockProducer)
 
-      val result = controller.generateNewCase(
+      val result = model.generateNewCase(
         theme = None,
         difficulty = Constraint.Difficulty.Medium
       ).unsafeRunSync()
 
       result.isRight shouldBe true
-      model.lastConstraints should contain(Constraint.Difficulty.Medium)
-      model.lastConstraints should have size 1
+      mockProducer.lastConstraints should contain(Constraint.Difficulty.Medium)
+      mockProducer.lastConstraints should have size 1
 
     "include theme when provided in generateNewCase" in:
-      val model = MockCaseGenerationModel(_ => IO.pure(Right(mockCase)))
-      val controller = CaseGenerationController(model)
+      val mockProducer = new MockProducer(_ => Right(mockCase))
+      val (model, _) = createTestEnvironment(mockProducer)
 
-      val result = controller.generateNewCase(
+      val result = model.generateNewCase(
         theme = Some("Mystery"),
         difficulty = Constraint.Difficulty.Easy
       ).unsafeRunSync()
 
       result.isRight shouldBe true
-      model.lastConstraints should contain(Constraint.Difficulty.Easy)
-      model.lastConstraints should contain(Constraint.Theme("Mystery"))
+      mockProducer.lastConstraints should contain(Constraint.Difficulty.Easy)
+      mockProducer.lastConstraints should contain(Constraint.Theme("Mystery"))
+      mockProducer.lastConstraints should have size 2
 
     "include custom constraints in generateNewCase" in:
-      val model = MockCaseGenerationModel(_ => IO.pure(Right(mockCase)))
-      val controller = CaseGenerationController(model)
+      val mockProducer = new MockProducer(_ => Right(mockCase))
+      val (model, _) = createTestEnvironment(mockProducer)
       val customConstraint = Constraint.CaseFilesRange(5, 10)
 
-      val result = controller.generateNewCase(
+      val result = model.generateNewCase(
         theme = None,
         difficulty = Constraint.Difficulty.Hard,
         customConstraints = Seq(customConstraint)
       ).unsafeRunSync()
 
       result.isRight shouldBe true
-      model.lastConstraints should contain allOf (
-        Constraint.Difficulty.Hard,
-        customConstraint
-      )
+      mockProducer.lastConstraints should contain(Constraint.Difficulty.Hard)
+      mockProducer.lastConstraints should contain(customConstraint)
+      mockProducer.lastConstraints should have size 2
 
-    "return error when model fails in generateNewCase" in:
-      val error = ProductionError.LLMError("LLM service failed")
-      val model = MockCaseGenerationModel(_ => IO.pure(Left(error)))
-      val controller = CaseGenerationController(model)
+    "pass all constraints correctly when using difficulty and theme" in:
+      val mockProducer = new MockProducer(_ => Right(mockCase))
+      val (model, _) = createTestEnvironment(mockProducer)
 
-      val result = controller.generateNewCase(
+      val result = model.generateNewCase(
+        theme = Some("Noir"),
+        difficulty = Constraint.Difficulty.Hard
+      ).unsafeRunSync()
+
+      result.isRight shouldBe true
+      mockProducer.lastConstraints should contain(Constraint.Difficulty.Hard)
+      mockProducer.lastConstraints should contain(Constraint.Theme("Noir"))
+      mockProducer.lastConstraints should have size 2
+
+    "work with only difficulty constraint when no theme or custom constraints provided" in:
+      val mockProducer = new MockProducer(_ => Right(mockCase))
+      val (model, _) = createTestEnvironment(mockProducer)
+
+      val result = model.generateNewCase(
         theme = None,
         difficulty = Constraint.Difficulty.Easy
       ).unsafeRunSync()
 
-      result.left.value shouldBe error
-
-    "pass constraints correctly in generateCaseWithConstraints" in:
-      val model = MockCaseGenerationModel(_ => IO.pure(Right(mockCase)))
-      val controller = CaseGenerationController(model)
-      val constraints = Seq(
-        Constraint.Difficulty.Hard,
-        Constraint.Theme("Noir")
-      )
-
-      val result =
-        controller.generateCaseWithConstraints(constraints).unsafeRunSync()
-
       result.isRight shouldBe true
-      model.lastConstraints shouldBe constraints
-
-    "handle empty constraints in generateCaseWithConstraints" in:
-      val model = MockCaseGenerationModel(_ => IO.pure(Right(mockCase)))
-      val controller = CaseGenerationController(model)
-
-      val result =
-        controller.generateCaseWithConstraints(Seq.empty).unsafeRunSync()
-
-      result.isRight shouldBe true
-      model.lastConstraints shouldBe empty
+      mockProducer.lastConstraints should contain only Constraint.Difficulty.Easy
