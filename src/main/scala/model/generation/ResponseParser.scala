33f9968cf3711c64ba574e70e3b8f9fd62a7b567
LucaSamore <luca.samore.01@gmail.com>
2025-11-10 01:35:33 +0100

    refactor(case-generation): response parser

--- src/main/scala/model/generation/ResponseParser.scala
+++ src/main/scala/model/generation/ResponseParser.scala
@@ -4,29 +4,20 @@ import model.game
 import model.game.{Case, CaseImpl, Hint, HintImpl}
 import upickle.default.*
 
+import scala.util.Try
+
 trait ResponseParser[T]:
   def parse(jsonString: String): Either[ProductionError, T]
 
 object ResponseParser:
 
+  private def parseString[T: Reader](jsonString: String): Either[ProductionError, T] =
+    Try(read[T](jsonString)).toEither.left.map { e =>
+      ProductionError.ParseError(s"Unexpected error during parsing: ${e.getMessage}\n${e.getStackTrace.mkString("\n")}")
+    }
+
   given ResponseParser[Case] with
-    override def parse(jsonString: String): Either[ProductionError, Case] =
-      try
-        println(jsonString)
-        Right(read[CaseImpl](jsonString))
-      catch
-        case e: Exception =>
-          Left(ProductionError.ParseError(
-            s"Unexpected error during parsing: ${e.getMessage}\n${e.getStackTrace.mkString("\n")}"
-          ))
+    override def parse(jsonString: String): Either[ProductionError, Case] = parseString[CaseImpl](jsonString)
 
   given ResponseParser[Hint] with
-    override def parse(jsonString: String): Either[ProductionError, Hint] =
-      try
-        print(jsonString)
-        Right(read[HintImpl](jsonString))
-      catch
-        case e: Exception =>
-          Left(ProductionError.ParseError(
-            s"Unexpected error during parsing: ${e.getMessage}\n${e.getStackTrace.mkString("\n")}"
-          ))
+    override def parse(jsonString: String): Either[ProductionError, Hint] = parseString[HintImpl](jsonString)
