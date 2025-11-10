ca3393cbdb0a0616835f9c1cca033cc0b2975b0e
LucaSamore <luca.samore.01@gmail.com>
2025-11-10 17:06:30 +0100

    fix(case-generation): test with new json template

--- src/main/scala/model/generation/ResponseParser.scala
+++ src/main/scala/model/generation/ResponseParser.scala
@@ -11,13 +11,13 @@ trait ResponseParser[T]:
 
 object ResponseParser:
 
-  private def parseString[T: Reader](jsonString: String): Either[ProductionError, T] =
-    Try(read[T](jsonString)).toEither.left.map { e =>
-      ProductionError.ParseError(s"Unexpected error during parsing: ${e.getMessage}\n${e.getStackTrace.mkString("\n")}")
-    }
-
   given ResponseParser[Case] with
     override def parse(jsonString: String): Either[ProductionError, Case] = parseString[CaseImpl](jsonString)
 
   given ResponseParser[Hint] with
     override def parse(jsonString: String): Either[ProductionError, Hint] = parseString[HintImpl](jsonString)
+
+  private def parseString[T: Reader](jsonString: String): Either[ProductionError, T] =
+    Try(read[T](jsonString)).toEither.left.map { e =>
+      ProductionError.ParseError(s"Unexpected error during parsing: ${e.getMessage}\n${e.getStackTrace.mkString("\n")}")
+    }
