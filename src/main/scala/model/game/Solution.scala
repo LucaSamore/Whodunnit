0547c60191263c10039a64c52a86796aff2b7cba
Lucia Castellucci <lucia.castellucci2@studio.unibo.it>
2025-11-12 16:25:58 +0100

    feat(solution): model solution submission states and validation results (refs #29)

---
+++ src/main/scala/model/game/Solution.scala
@@ -0,0 +1,33 @@
+package model.game
+
+object ValidationResult:
+  def validateSolution(
+      accusedCharacter: Character,
+      solution: Solution
+  ): ValidationResult =
+    if accusedCharacter == solution.culprit then
+      CorrectSolution(solution.culprit, solution.motive)
+    else
+      IncorrectSolution(accusedCharacter, solution.culprit, solution.motive)
+
+enum SubmissionState:
+  case NotSubmitted
+  case Submitting(character: Character)
+  case Submitted(result: ValidationResult)
+
+enum ValidationResult:
+  case PrerequisitesNotMet
+
+  case PrerequisitesMet
+
+  case CorrectSolution(culprit: Character, motive: String)
+
+  case IncorrectSolution(
+      accusedCharacter: Character,
+      actualCulprit: Character,
+      motive: String
+  )
+
+object SolutionConfig:
+  val PrerequisiteCoverageThreshold: Double = 1.0
+  val TimeElapsedThreshold: Double = 0.85
